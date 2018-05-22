6f5865776b356d41c5ed7fd204765a0f1d5269d6
kirill-kundik <goliathusua@gmail.com>
2018-05-22 12:06:38 +0300

    Lab1 done
    attempts to forbid jumping in mid air

---
+++ Assets/Scripts/RabbitBehaviorScript.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+public class RabbitBehaviorScript : MonoBehaviour {
+
+    public float Speed = 1;
+    private Rigidbody2D _bunny;
+
+	private bool _isGrounded = true;
+	
+	// Use this for initialization
+	void Start () {
+		_bunny = GetComponent<Rigidbody2D>();
+	}
+	
+	void FixedUpdate() {
+	
+	    float value = Input.GetAxis("Horizontal");
+
+		if (Input.GetKeyDown(KeyCode.Space) && _isGrounded)
+		{
+			Vector2 vel = _bunny.velocity;
+			vel.y = Speed;
+			_bunny.velocity = vel;
+
+			_isGrounded = false;
+		}
+
+		SpriteRenderer sr = GetComponent<SpriteRenderer>();
+		if (value > 0)
+			sr.flipX = false;
+		else if (value < 0)
+			sr.flipX = true;
+
+		if (Mathf.Abs(value) > 0)
+		{
+			Vector2 vel = _bunny.velocity;
+			vel.x = value * Speed;
+			_bunny.velocity = vel;
+		}
+
+	}
+
+	void OnCollisionEnter(Collision other)
+	{
+		_isGrounded = true;
+	}
+
+	void OnTriggerEnter(Collision other)
+	{
+		_isGrounded = true;
+	}
+}
---
+++ Assets/Scripts/RabbitCameraScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class RabbitCameraScript : MonoBehaviour
+{
+    public Transform Rabbit;
+
+    private Vector2 velocity;
+    
+    public float SmoothTimeX;
+    public float SmoothTimeY;
+
+    void FixedUpdate()
+    {
+        float posX = Mathf.SmoothDamp(transform.position.x, Rabbit.position.x, ref velocity.x, SmoothTimeX);
+        float posY = Mathf.SmoothDamp(transform.position.y, Rabbit.position.y, ref velocity.y, SmoothTimeY);
+        
+        transform.position = new Vector3(posX, posY, transform.position.z);
+        
+    }
+}
