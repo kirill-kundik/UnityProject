b951d41b71ec9534d7c6f5d52afd1aa4ce6a235c
kirill-kundik <goliathusua@gmail.com>
2018-05-23 11:28:45 +0300

    fixed jumps in the air
    fixed jumps in the air

--- Assets/Scripts/RabbitBehaviorScript.cs
+++ Assets/Scripts/RabbitBehaviorScript.cs
@@ -1,52 +1,58 @@
 ﻿using UnityEngine;
 
-public class RabbitBehaviorScript : MonoBehaviour {
-
+public class RabbitBehaviorScript : MonoBehaviour
+{
     public float Speed = 1;
     private Rigidbody2D _bunny;
 
-	private bool _isGrounded = true;
-	
-	// Use this for initialization
-	void Start () {
-		_bunny = GetComponent<Rigidbody2D>();
-	}
-	
-	void FixedUpdate() {
-	
-	    float value = Input.GetAxis("Horizontal");
+    private bool _isGrounded = true;
 
-		if (Input.GetKeyDown(KeyCode.Space) && _isGrounded)
-		{
-			Vector2 vel = _bunny.velocity;
-			vel.y = Speed;
-			_bunny.velocity = vel;
+    // Use this for initialization
+    void Start()
+    {
+        _bunny = GetComponent<Rigidbody2D>();
+    }
 
-			_isGrounded = false;
-		}
+    void FixedUpdate()
+    {
+        float value = Input.GetAxis("Horizontal");
 
-		SpriteRenderer sr = GetComponent<SpriteRenderer>();
-		if (value > 0)
-			sr.flipX = false;
-		else if (value < 0)
-			sr.flipX = true;
+        if (Input.GetKeyDown(KeyCode.Space) && _isGrounded)
+        {
+            Vector2 vel = _bunny.velocity;
+            vel.y = Speed;
+            _bunny.velocity = vel;
 
-		if (Mathf.Abs(value) > 0)
-		{
-			Vector2 vel = _bunny.velocity;
-			vel.x = value * Speed;
-			_bunny.velocity = vel;
-		}
+            _isGrounded = false;
+        }
 
-	}
+        SpriteRenderer sr = GetComponent<SpriteRenderer>();
+        if (value > 0)
+            sr.flipX = false;
+        else if (value < 0)
+            sr.flipX = true;
 
-	void OnCollisionEnter(Collision other)
-	{
-		_isGrounded = true;
-	}
+        if (Mathf.Abs(value) > 0)
+        {
+            Vector2 vel = _bunny.velocity;
+            vel.x = value * Speed;
+            _bunny.velocity = vel;
+        }
+    }
 
-	void OnTriggerEnter(Collision other)
-	{
-		_isGrounded = true;
-	}
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.collider.gameObject.CompareTag("ground"))
+        {
+            _isGrounded = true;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.collider.gameObject.CompareTag("ground"))
+        {
+            //_isGrounded = false;
+        }
+    }
 }
