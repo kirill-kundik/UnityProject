d868aefbd6a724663292a68103457ebd951df47b
kirill-kundik <goliathusua@gmail.com>
2018-05-27 13:46:21 +0300

    added level controller and rabbit lifes counter

---
+++ Assets/Scripts/LevelController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelController : MonoBehaviour
+{
+
+	public static LevelController Current;
+	public int LifesCounter = 3;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void Awake()
+	{
+		Current = this;
+	}
+
+	int GetLifesCounter()
+	{
+		return LifesCounter;
+	}
+}
