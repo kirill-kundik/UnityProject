522a848f1d791217119262e2176345284d8f8f30
kirill-kundik <goliathusua@gmail.com>
2018-06-18 16:52:10 +0300

    moving platform behavior created

---
+++ Assets/Scripts/MovingPlatformScript.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+public class MovingPlatformScript : MonoBehaviour
+{
+
+	public Vector3 MoveBy;
+	public float Speed = 0.5f;
+	public float TimeToWait = 1f;
+	
+	private Vector3 _pointA;
+	private Vector3 _pointB;
+	private bool _isGoingToA;
+
+	private float _timeOut;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		_pointA = transform.position;
+		_pointB = _pointA + MoveBy;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 myPos = transform.position;
+		Vector3 target = _isGoingToA ? _pointA : _pointB;
+
+		if (IsArrived(myPos, target))
+		{
+			_timeOut = TimeToWait;
+			_isGoingToA = !_isGoingToA;
+		}
+		else
+		{
+			if (_timeOut > 0)
+			{
+				_timeOut -= Time.deltaTime;
+			}
+			else
+			{
+				myPos = Vector3.MoveTowards(myPos, target, Speed);
+			}
+		}
+
+		transform.position = myPos;
+	}
+
+	bool IsArrived(Vector3 pos, Vector3 target)
+	{
+		pos.z = 0;
+		target.z = 0;
+		return Vector3.Distance(pos, target) < 0.02f;
+	}
+}
